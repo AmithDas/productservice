17ecb007a21b5595d44de4cb9d8a25fc2dd8eede
Amith Das <amithdas89@gmail.com>
2022-05-10 14:33:36 +0530

    Enabling feign client in product service

--- src/main/java/com/amith/springcloud/controller/ProductController.java
+++ src/main/java/com/amith/springcloud/controller/ProductController.java
@@ -6,19 +6,26 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.amith.springcloud.model.Coupon;
 import com.amith.springcloud.model.Product;
 import com.amith.springcloud.repos.ProductRepository;
+import com.amith.springcloud.restclient.CouponClient;
 
 @RestController
 @RequestMapping("/productapi")
 public class ProductController {
-	
+
 	@Autowired
 	private ProductRepository productRepository;
-	
+
+	@Autowired
+	private CouponClient couponClient;
+
 	@PostMapping(value = "/products")
 	public Product create(@RequestBody Product product) {
+		Coupon coupon = couponClient.getCoupon(product.getCouponCode());
+		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
 		return productRepository.save(product);
 	}
-	
+
 }
