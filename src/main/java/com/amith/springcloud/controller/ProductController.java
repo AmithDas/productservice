5433d4245bd52af94052ce7a51a23dd1ed2fff89
Amith Das <amithdas89@gmail.com>
2022-05-12 17:06:56 +0530

    Enabling cloud bus

--- src/main/java/com/amith/springcloud/controller/ProductController.java
+++ src/main/java/com/amith/springcloud/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.amith.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 @RequestMapping("/productapi")
+@RefreshScope
 public class ProductController {
 
 	@Autowired
@@ -23,12 +27,20 @@ public class ProductController {
 	@Autowired
 	private CouponClient couponClient;
 
+	@Value("${com.amith.property}")
+	private String prop;
+
 	@PostMapping(value = "/products")
 	@Retry(name = "product-api", fallbackMethod = "handleError")
 	public Product create(@RequestBody Product product) {
 		Coupon coupon = couponClient.getCoupon(product.getCouponCode());
 		product.setPrice(product.getPrice().subtract(coupon.getDiscount()));
 		return productRepository.save(product);
+	}
+	
+	@GetMapping(value = "/prop")
+	public String getProp() {
+		return this.prop;
 	}
 	
 	public Product handleError(Product product, Exception exception) {
