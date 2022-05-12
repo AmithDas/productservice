76fc5ecc95089e0520a364fcba3aca7a3d64fc0f
Amith Das <amithdas89@gmail.com>
2022-05-12 15:33:12 +0530

    Fixing Junit test cases

--- src/test/java/com/amith/springcloud/ProductserviceApplicationTests.java
+++ src/test/java/com/amith/springcloud/ProductserviceApplicationTests.java
@@ -2,8 +2,10 @@ package com.amith.springcloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:/product-service-test.properties")
 class ProductserviceApplicationTests {
 
 	@Test
